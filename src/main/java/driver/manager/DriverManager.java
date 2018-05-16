14aefcbdc3c53871d0e0003811fbbf90e9bc440b
Robert Hope <robby_hope@hotmail.com>
2018-05-16 15:49:44 +0100

    Re Adding IE Driver Experiments and Driver Manager Class

--- src/main/java/driver/manager/DriverManager.java
+++ src/main/java/driver/manager/DriverManager.java
@@ -1,32 +1,38 @@
 package driver.manager;
 
-import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+        import org.openqa.selenium.ie.InternetExplorerDriver;
 
 
 public class DriverManager {
 
 
+    public static final String SELENIUM2_BASICS_DRIVER = "selenium2Basics.driver";
+
     public static WebDriver get() {
         String browserToUse ="";
-        //in order for our driver manager to decide which driver to use whe need to create an if statement
+        //in order for our driver manager to decide which driver to use we need to create an if statement
         //which assesses the system properties set, it uses the result of that to set a string browserToUse
 
-        if(System.getProperties().containsKey("selenium2Basics.driver")){
+        if(System.getProperties().containsKey(SELENIUM2_BASICS_DRIVER)){
             //set browserToUse to the property passed in by the test, if nothing s passed in
-            //nothing gets set
-             browserToUse = System.getProperty("selenium2Basics.driver");
+            //nothing gets set and we use the default browser (see below)
+            browserToUse = System.getProperty(SELENIUM2_BASICS_DRIVER);
         }
 
-        //now use a switch statment to determine which browser we want to return, based on the system property set
+        //now use a switch statement to determine which browser we want to return, based on the system property set
 
         switch(browserToUse) {
             case "firefox":
                 return new FirefoxDriver();
+            case "ie":
+                return new InternetExplorerDriver();
             default:
                 return new ChromeDriver();
 
+
         }
-     }
+    }
 }
--- src/test/java/DriverManagerTests/NavigateUsingDriverManagerClassTest.java
+++ src/test/java/DriverManagerTests/NavigateUsingDriverManagerClassTest.java
@@ -2,11 +2,8 @@ package DriverManagerTests;
 
 import driver.manager.DriverManager;
 import org.junit.After;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,28 +11,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NavigateUsingDriverManagerClassTest {
 
-    // 1 . set static web driver to use in the the class
-    static WebDriver driver;
+
+    public static final String SELENIUM2_BASICS_DRIVER = "selenium2Basics.driver";
+    WebDriver driver;
+
 
     //2 In our at before class we want to run a get method which returns the type of driver we wish to use in out tests
 //start building this out into an abstract class
-    @BeforeClass
-    public static void getDriver() {
-// this get acts on the driver class and gets the driver type NOT a url
-        // dont get this confused with the get method which acts on a WebDriver Object
-        driver = DriverManager.get();
-    }
+
     //3. start writing a simple test which runs a browser and navigates to it using FF
     @Test
     public void createAFFDriver(){
 
-        System.setProperty("selenium2Basics.driver", "firefox");
+        System.setProperty(SELENIUM2_BASICS_DRIVER, "firefox");
 
        assertBrowserTestRuns();
     }
 
     //4. do the same as above but use a default driver which is used in if we dont set a driver in system properties
-    //also refactor repeated code
+    //also refactor repeated code. note ive used chrome as the default driver
     @Test
     public void createADefaultDriver(){
 
@@ -44,8 +38,24 @@ public class NavigateUsingDriverManagerClassTest {
         assertBrowserTestRuns();
     }
 
+    @Test
+    public void createAnIeDriverTest(){
+        //set the driver location string
+        String ieDriverLocationLGC = ("C:\\Automation Tools\\IEDriver\\IEDriverServer.exe");
+        //use the string to set the driver property
+        System.setProperty("webdriver.ie.driver", ieDriverLocationLGC);
+
+        //set the property to call a new ie driver instance
+        System.setProperty(SELENIUM2_BASICS_DRIVER, "ie");
+
+        assertBrowserTestRuns();
+
+
+    }
+
 
     public void assertBrowserTestRuns(){
+        driver = DriverManager.get();
         driver.get("http://compendiumdev.co.uk/selenium/basic_web_page.html");
         assertThat(driver.getTitle(),is("Basic Web Page Title"));
 
@@ -54,7 +64,6 @@ public class NavigateUsingDriverManagerClassTest {
 
     @After
     public void quitDriver(){
-
         driver.quit();
     }
 
